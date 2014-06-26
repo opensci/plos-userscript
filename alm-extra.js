9317e65c68e17d9554186c9bff0807a504857add
Mike Chelen <michael.chelen@gmail.com>
2014-06-26 05:59:50 -0400

    first commit

---
+++ alm-extra.js
@@ -0,0 +1,19 @@
+// ==UserScript== //
+
+// @name Hello World
+
+// @namespace http://www.plosone.org
+
+// @description Test
+
+// @include *
+
+// ==/UserScript==
+
+// test
+
+
+$('#article-metrics').append(
+    $('<div>').html('Hello World'),
+);
+
